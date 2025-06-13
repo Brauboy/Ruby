df650d2ea00cf627918627bf441210c431d34943
Brauboy <brauliopablofg@gmail.com>
2025-06-13 13:01:19 -0300

    Primeiro Projeto

---
+++ Projeto01.rb
@@ -0,0 +1,10 @@
+puts "Digite seu Primeiro número: "
+nome = gets.chomp
+
+puts "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+puts "Digite sua idade: "
+idade = gets.chomp.to_i
+
+puts " Oi, #{nome} #{sobrenome} você tem #{idade} anos. Seja bem vindo!"
