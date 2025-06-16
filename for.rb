b00ca6c9f462b6d94c70acfe903ee70266e436ed
Brauboy <brauliopablofg@gmail.com>
2025-06-16 16:34:19 -0300

    Estruturas de controle iterativo FOR

---
+++ for.rb
@@ -0,0 +1,14 @@
+# for = durante
+# leia uma lista (array) de elementos - linguagem de programação
+# primeiro- criar a lista
+
+linguagens = ["Ruby", "Go", "C++", "Python"]
+count = 1
+
+#aparecça na tela essa lista
+#laço For
+for linguagem in linguagens 
+    puts linguagem
+    puts count
+    count = count + 1
+end
