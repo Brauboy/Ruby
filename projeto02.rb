d582a65db3a02e6d338c0d3ef7526ddea6246904
Brauboy <brauliopablofg@gmail.com>
2025-06-20 14:52:46 -0300

    Pojeto elevado a potência com array

--- projeto02.rb
+++ projeto02.rb
@@ -3,8 +3,17 @@ numeros = []
 puts "Digite o primeiro número: "
 primeiro = gets.chomp.to_i
 numeros.push(primeiro) 
-numeros.map! do |x|
-     x**3
-end
 
+
+puts "Digite o segundo número: "
+segundo = gets.chomp.to_i
+numeros.push(segundo) 
+
+
+puts "Digite o terceiro número: "
+terceiro = gets.chomp.to_i
+numeros.push(terceiro) 
+
+numeros.map! { |x| x**3 }
+     
 puts numeros
