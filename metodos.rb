288e21927dea963abe5744eb3c439aa6486bd915
Brauboy <brauliopablofg@gmail.com>
2025-06-25 11:43:57 -0300

    métodos

---
+++ metodos.rb
@@ -0,0 +1,15 @@
+# def oi
+#   puts "Oi Dev!"
+# end
+# oi
+
+def nome (nome,sobrenome)
+  puts" Oi #{nome} #{sobrenome}, você é um Dev em Ruby!"
+end
+
+nome = 'Braulio'
+sobrenome = 'Pablo'
+
+nome(nome,sobrenome)
+nome(nome,sobrenome)
+nome(nome,sobrenome)
