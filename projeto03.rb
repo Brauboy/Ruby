75a16a2531d87b4e7fd54b4b90edbabc2e86b199
brauliopablo <brauliopablofg@gmail.com>
2025-06-27 15:49:16 -0300

    Consulta de CPF

--- projeto03.rb
+++ projeto03.rb
@@ -1,5 +1,19 @@
-require 
+require "cpf_cnpj"
+
+
+def check_cpf(cpf)
+
+    if CPF.valid?(cpf)
+        return "O cpf informado é válido"
+    else
+        return "O cpf informado é inválido"
+    end
+end
 
 puts "Digite um CPF:"
 cpf = gets.chomp.to_i
+
+result = check_cpf(cpf)
+
+puts result
 
