f524af24a54f70d1cc484c66d8871633a65e9273
brauliopablo <brauliopablofg@gmail.com>
2025-08-06 15:47:49 -0300

    Expressoes regulares - method_missing

---
+++ mm.rb
@@ -0,0 +1,17 @@
+#method_missing
+
+class Fish
+
+    def method_missing(method_name)
+        puts "O peixe não pode #{method_name} ainda"
+    end
+
+    def nadar
+        puts 'O peixe está nadando'
+    end
+end
+
+fish = Fish.new
+fish.nadar
+fish.andar
+fish.chutar
