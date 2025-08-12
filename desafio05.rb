a0c675555cfe8b0cdd454a50bc1f35e484597eda
brauliopablo <brauliopablofg@gmail.com>
2025-08-12 17:01:37 -0300

    Desafio 05 - imposto do gás

--- desafio05.rb
+++ desafio05.rb
@@ -1,15 +1,18 @@
-imposto          = 0.1
-preco_do_gas     = gets.chomp.to_i      
-imposto_cobrado  = gets.chomp.to_i
-imposto_variavel = gets.chomp.to_f
-   
+puts "Digite o preço do gás:"
+preco_gas = gets.chomp.to_f
 
-valor_total = preco_do_gas * (imposto * (preco_do_gas / 100.0) * 100)
+puts "O imposto variável será cobrado? (0 - não, 1 - sim):"
+tem_imposto_variavel = gets.chomp.to_i
 
-if imposto_cobrado == 1 then
-  valor_total += (valor_total / 100.0) * imposto_variavel
+puts "Digite o valor do imposto variável (%):"
+valor_imposto_variavel = gets.chomp.to_f
+
+preco_com_imposto_fixo = preco_gas * 1.10
+
+if tem_imposto_variavel == 1
+  preco_final = preco_com_imposto_fixo * (1 + valor_imposto_variavel / 100)
+else
+  preco_final = preco_com_imposto_fixo
 end
 
-valor_total = valor_total.to_s.chop.to_i if valor_total.to_s[-1] =="0"
-
-puts "O preço do gás nesse mês é de R$#{valor_total}"
+puts "O preço do gás nesse mês é de R$#{'%.2f' % preco_final}"
